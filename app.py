8f5732bf7032ce2f94fb748e6756427bbe173a4d
MinhTriet_19120039 <57978985+19120039@users.noreply.github.com>
2023-06-15 18:26:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,46 @@
 import streamlit as st
+from transformers import BertForQuestionAnswering, BertTokenizer
+import torch
 
-# Tiêu đề
-st.title("Question Answering Demo")
+def answer_question(context, question):
+    # Load the trained model and tokenizer
+    model = BertForQuestionAnswering.from_pretrained('trained_model')
+    tokenizer = BertTokenizer.from_pretrained('trained_model')
 
-# Input question và context
-question = st.text_input("Question:", "What is Beyoncé's hometown?")
-context = st.text_area("Context:", "Beyoncé was born and raised in Houston, Texas.")
-
-# Button để thực hiện dự đoán
-if st.button("Predict"):
-    # Tiến hành dự đoán và hiển thị kết quả
+    # Tokenize question and context
     inputs = tokenizer.encode_plus(question, context, add_special_tokens=True, return_tensors="pt")
+
+    # Forward pass through the model
     with torch.no_grad():
         outputs = model(**inputs)
+
+    # Get the predicted start and end positions
     start_logits = outputs.start_logits
     end_logits = outputs.end_logits
+
+    # Find the tokens with the highest probability
     start_index = torch.argmax(start_logits)
     end_index = torch.argmax(end_logits)
+
+    # Get the predicted answer span
     tokens = tokenizer.convert_ids_to_tokens(inputs["input_ids"].squeeze().tolist())
     answer = tokenizer.convert_tokens_to_string(tokens[start_index:end_index+1])
-    st.success("Predicted Answer: " + answer)
+
+    return answer
+
+def main():
+    st.title("Question Answering Demo")
+
+    # Input question and context
+    question = st.text_input("Enter your question:")
+    context = st.text_area("Enter the context:")
+
+    if st.button("Get Answer"):
+        if context and question:
+            answer = answer_question(context, question)
+            st.success("Predicted Answer: {}".format(answer))
+        else:
+            st.warning("Please enter both question and context.")
+
+if __name__ == "__main__":
+    main()
